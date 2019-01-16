f71107b712c9e79d668151bbe47b5fe9f3870f51
yulianiamaliah <46737066+yulianiamaliah@users.noreply.github.com>
2019-01-16 12:07:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
-<?php  
+<?php 
 
-require_once 'App/init.php';
+require_once 'app/init.php';
 
-// $produk1 = new Game("Naruto","Masashi Kishimoto","Shoenin Jump",300000, 0, 50);
-// $produk2 = new Novel("Hujan","Tere Liye","PT Gramedia", 36000, 300, 0);
+$produk1 = new Game("Naruto","Masashi Kishimoto","Shoenin Jump",300000, 0, 50);
+$produk2 = new Novel("Bulan","Tere Liye","PT Gramedia", 36000, 300, 0);
 
-// $cetakProduk = new cetakInfoProduk();
-// $cetakProduk->tambahProduk( $produk1 );
-// $cetakProduk->tambahProduk( $produk2 );
+$cetakProduk = new cetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
 
-// echo $cetakProduk->cetak();
+echo $cetakProduk->cetak();
+echo "<hr>"
 
-// echo"<hr>";
-use App\Service\user as Serviceuser;
-use App\Produk\user as Produkuser;
-
-
-new Serviceuser();
-echo "<br>";
-new Produkuser();
+ ?>
