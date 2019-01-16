648cf0c794deaa19ed3e3841f310725a925ee25f
yulianiamaliah <46737066+yulianiamaliah@users.noreply.github.com>
2019-01-16 12:07:34 +0700

    Add files via upload

--- cetakInfoProduk.php
+++ cetakInfoProduk.php
@@ -1,18 +1,18 @@
 <?php 
-class cetakInfoProduk{
-	public $daftarProduk = array();
+ class cetakInfoProduk{
+public $daftarProduk = array();
 
-	public function tambahProduk (Produk $produk){
-		$this->daftarProduk[] = $produk;
+public function tambahProduk (Produk $produk){
+	$this->daftarProduk[] = $produk;
 }
 
 public function cetak(){
-	$str ="DAFTAR PRODUK : <br>";
+		$str ="DAFTAR PRODUK : <br>";
 
-	foreach($this->daftarProduk as $p){
-		$str .= "- {$p->getInfoProduk()} <br>";
-	}
+        foreach($this->daftarProduk as $p){
+        	$str .= "- {$p->getInfoProduk()} <br>";
+        }
 
-	return $str;
+		return $str;
 	}
 }
